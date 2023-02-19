862160e781a4ec750fdeb7cf92aa60c7932ed05c
Leonard <leoa7630@gmail.com>
2023-02-19 12:19:23 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,7 @@
  *return: 0 if success.
  */
 int main(void)
-printf("with proper grammar, but the outcome is a piece of art,/n");
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
